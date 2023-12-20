a5934cef8571c7f3e87098a8e5ab57671c63949e
Alessandro Bezerra <alessandrobezerra100@gmail.com>
2023-12-19 22:59:26 -0300

    More
    +console
    +camera
    +notas

--- Scripts/Cams/change_cam.gd
+++ Scripts/Cams/change_cam.gd
@@ -1,5 +1,5 @@
 extends Camera3D
-
+class_name Chage
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Scripts/computers/bash.gd
+++ Scripts/computers/bash.gd
@@ -15,7 +15,7 @@ var respostas_corretas = ["1","2","3"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	input.grab_focus()
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	prompt.text += get_menu()
 	
@@ -65,6 +65,8 @@ func _process(delta):
 				"back":
 					prompt.text += back()
 					prompt.text += get_relatorios()
+				"sair":
+					get_tree().change_scene_to_file("res://scenes/alan_scenen.tscn")
 				_:
 					
 					

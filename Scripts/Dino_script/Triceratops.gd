0e3a61b17e5bfd990e700d33d073b516560d08f1
Alan Souza <rayooszmr@gmail.com>
2023-12-20 01:48:02 -0300

    newdinos
    dinos with animation

---
+++ Scripts/Dino_script/Triceratops.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@onready var animation = $AnimationPlayer
+@export var pathFollow3D: PathFollow3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#animation.play_backwards("TRex_Walk")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	animation.play("Triceratops_Walk")
+	#const move_speed := 10
+	
+	#pathFollow3D.progress += move_speed * delta
+	pass

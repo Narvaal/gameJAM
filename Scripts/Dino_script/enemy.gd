ea281943a0e45b6e6ef8ad3b3296d4c0b5936804
Alessandro Bezerra <alessandrobezerra100@gmail.com>
2023-12-23 23:10:32 -0300

    fix

--- Scripts/Dino_script/enemy.gd
+++ Scripts/Dino_script/enemy.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var animation = $AnimationPlayer
 @export var pathFollow3D: PathFollow3D
+@export var volume:float = 0
+@export var rng_max = 20000
 var rng = RandomNumberGenerator.new()
 
 
@@ -9,13 +11,15 @@ var can_be_visible = false
 @onready var palmtop = get_node("/root/AlanScenen/Player/screen_player/screen_colision/screen_image")
 @onready var audio = get_node("/root/AlanScenen/Player/screen_player/AudioStreamPlayer3D")
 @onready var timer = get_node("/root/AlanScenen/Room/computer/screens/Timer")
+
 var sfx
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#sfx = load("res://Songs/jumpscare.wav") 
-	sfx = load("res://Songs/button.mp3") 
+	sfx = load("res://Songs/jumpscare.wav") 
+	#sfx = load("res://Songs/button.mp3") 
 	audio.stream = sfx
+	audio.volume_db = volume
 	self.visible = false
 	pass
 
@@ -24,32 +28,13 @@ func _ready():
 func _process(delta):
 	animation.play("idle")
 
-	
 	if can_be_visible:
 		rng.randomize()
-		if rng.randi_range(0,20000) == 1:
+		if rng.randi_range(0,rng) == 1:
 			self.visible = true
 			audio.play()
 			can_be_visible = false
 			timer.start()
-	
-			
-	#if self.visible:
-		
-			
-			
-		#if palmtop.visible == true:
-			#
-			#audio.stream = sfx
-			#audio.play()
-		#
-		#
-	#rng.randomize()
-	#if rng.randi_range(0,300) == 1:
-		#self.visible = false
-	
-	#pathFollow3D.progress += move_speed * delta
-	pass
 
 
 func _on_timer_timeout():
